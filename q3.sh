85dd5a3f5b49902a4dbbe155148d11c1784b63bd
Srija M <82724355+sri-ja@users.noreply.github.com>
2022-04-19 19:19:34 +0530

    Add final solution for question 3

--- q3.sh
+++ q3.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+echo Part a
 wc -c $1 | awk '{print "File size (in bytes): " $1}'
 
+echo Part b
 wc -l $1 | awk '{print "Total number of lines: " $1}'
 
+echo Part c
 wc -w  $1 | awk '{print "Total number of words: " $1}'
 
+echo Part d
 linecount=1
 
 while read line; do
@@ -13,11 +17,46 @@ while read line; do
 	((linecount++))
 done < $1
 
-awk '{ print tolower($0) }' $1 > temp.txt
+awk '{ print tolower($0) }' $1 > temp0.txt
 
-grep -wo '[[:alnum:]]\+' temp.txt | sort | uniq -cd | awk '{print "Word: " $2 " - Count of repitition: " $1}'
+cat temp0.txt | tr -d '[:punct:]' > temp1.txt
 
-rm temp.txt
+echo Part e
+declare -a temparr
+present=0
+
+while read -ra line
+do
+	for word in "${line[@]}"
+	do
+		tempvar=$(grep -o -i $word temp0.txt | wc -l) 
+		if [ $tempvar -gt 1 ]
+		then
+			present=0
+			for i in "${temparr[@]}"
+			do
+				if [ "$word" == "$i" ]
+				then
+					present=1
+				fi
+			done
+			if [ $present -eq 0 ]
+			then
+				temparr+=($word)
+			fi
+		fi
+	done
+done < temp1.txt
+
+for word in "${temparr[@]}" 
+do
+	tempvar=$(grep -o -i $word temp0.txt | wc -l)
+	echo "Word:" $word "- Count of repitition:" $tempvar
+done
+
+rm temp0.txt
+rm temp1.txt
+
 
 
 
